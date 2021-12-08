6748cd5ff1903464057c9c39e7cceb8d4e3f5433
Gramos Hoxha <ProfessorHoxha@gmail.com>
2021-12-08 14:33:31 +0000

    Update textproto.py
    Google search operators coming along well and working. prototype nearly finished just a couple problems to solve

--- textproto.py
+++ textproto.py
@@ -8,16 +8,17 @@
 class Form(object):
     
 #Constructer
-    def __init__(self, first_name, last_name, username, organisation_name):
+    def __init__(self, first_name, last_name, username, organisation_name, prefix):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
-        self.organisation_name = organisation_name    
+        self.organisation_name = organisation_name
+        self.prefix = prefix
            
-#methods
+#data_methods
     def save_data(self):
-        file = open("database.txt", "w")
-        file.write(self.describe())
+        file = open("database.txt", "a")
+        file.write(self.data_organisation())
         file.close()
         
     def retreive_data(self):
@@ -25,26 +26,38 @@ class Form(object):
          retreived = file.readlines()
          newlist = []
          for line in retreived:
-             newlist.append(line.strip())      
+             newlist.append(line.strip())
+         file.close()
          return newlist
+
+    def data_organisation(self):
+        details = (f"\nFirst Name:{self.first_name} Last Name:{self.last_name} Username:{self.username} Organisation Name:{self.organisation_name}\n")
+        return details
         
     def describe(self):
         details = (f"First Name: {self.first_name}\nLast Name: {self.last_name}\nUsername: {self.username}\nOrganisation Name: {self.organisation_name}")
         return details
-
-#Inherited class
-class google_search(Form):
-
-#constructer
-    def __init__(self, first_name, last_name, username, organisation_name, prefix):
-        super().__init__(first_name, last_name, username, organisation_name)
-        self.prefix = prefix
-#methods
-    def organisation_and_filetype(self):
-        pass
+    
+#search_methods 
     def linkedin(self):
-        pass
+        linkedin_suffix = "Site%3Alinkedin.com"
+        url = (f"{self.prefix}+{self.first_name}+{self.last_name}+{linkedin_suffix}")
+        return url
+        
     def facebook(self):
-        pass
+        facebook_suffix = "Site%3Afacebook.com"
+        url = (f"{self.prefix}+{self.first_name}+{self.last_name}+{facebook_suffix}")
+        return url
+    
     def name_and_organisation(self):
-        pass
+        url = (f'{self.prefix}+"{self.first_name}+{self.last_name}"+{self.organisation_name}')
+        return url
+    
+    def organisation_and_filetype(self):
+        xlsx_suffix = "filetype%3Axlsx"
+        url = (f"{self.prefix}+{self.organisation_name}+{xlsx_suffix}")
+        return url
+    
+    def all_links(self):
+        links = (f"{self.linkedin()}\n{self.facebook()}\n{self.name_and_organisation()}\n{self.organisation_and_filetype()}")
+        return links
