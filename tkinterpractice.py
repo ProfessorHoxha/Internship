1e1d56a9fe124c738362083abff3ae71fa1839c4
Gramos Hoxha <ProfessorHoxha@gmail.com>
2021-12-15 14:30:27 +0000

    Update tkinterpractice.py

--- tkinterpractice.py
+++ tkinterpractice.py
@@ -1,39 +1,47 @@
 #https://www.pythontutorial.net/tkinter/tkinter-object-oriented-frame/
-#import library
+#Demystifying Cybercrime, The Risk management Group
+#https://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 
-class MainFrame(ttk.Frame):
+class Form_frame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
-
-        options = {'padx': 5, 'pady': 5}
-
-        #label
-        self.label = ttk.Label(self, text="Testing 123")
-        self.label.pack(**options)
         
-        #button
-        self.button = ttk.Button(self, text='Click Me', command=self.button_clicked)  
-        self.button.pack(**options)
+        options = {'padx': 0, 'pady': 200}
 
-        #show the frame on the container
+        label_one = ttk.Label(self, text='Form')
+        label_one.pack(**options)   
+
         self.pack(**options)
-
-    def button_clicked(self):
-        showinfo(title='Information',
-                 message='Hello, Tkinter!')
-
+         
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         # configure the root window
-        self.title('My Awesome App')
-        self.geometry('700x500')
+        self.title('TRMG')
+        self.geometry('800x500')
+    def Label(self):
+        #Demystifying Cybercrime
+        label_one = tk.Label(self, text="Demystifying Cybercrime", font=("Arial", 25))
+        label_one.place(x=200, y=0)
+        
+        #The Risk management Group
+        label_two = tk.Label(self, text="The Risk management Group", font=("Arial", 20))
+        label_two.place(x=200, y=40)
+
+    def image(self):
+        pass
 
 #mainloop
 if __name__ == "__main__":
     app = App()
-    frame = MainFrame(app)
+    # configure the root window
+    app.title('TRMG')
+    app.geometry('1000x800')
+    form = Form_frame(app)
+
+    app.Label()
     app.mainloop()
